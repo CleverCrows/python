3ae80b9b6753ba17b02553a00c871b9065d33f20
CleverCrows <151632383+CleverCrows@users.noreply.github.com>
2023-11-21 23:02:01 -0600

    Documentation added

--- television.py
+++ television.py
@@ -1,4 +1,8 @@
 class Television:
+    """
+    Sets the minimum and maximum range for volume
+    Sets the minimum and maximum number of channels
+    """
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
@@ -11,12 +15,18 @@ class Television:
         self.channel = Television.MIN_CHANNEL
 
     def power(self):
+        """
+        Turns the power on and off
+        """
         if self.status == False:
             self.status = True
         else:
             self.status = False
 
     def mute(self):
+        """
+        Mutes and unmutes the television
+        """
         if self.status == False:
             return
         elif self.muted == False:
@@ -25,6 +35,10 @@ class Television:
             self.muted = False
 
     def channel_up(self):
+        """
+        Changes to the next channel
+        If it is already the last channel, goes back to the first channel
+        """
         if self.status == False:
             return
         elif self.channel == Television.MAX_CHANNEL:
@@ -33,6 +47,10 @@ class Television:
             self.channel += 1
 
     def channel_down(self):
+        """
+        Changes to the previous channel
+        If it is already the first channel, goes to the last channel"
+        """
         if self.status == False:
             return
         elif self.channel == Television.MIN_CHANNEL:
@@ -41,6 +59,10 @@ class Television:
             self.channel -= 1
 
     def volume_up(self):
+        """
+        Increases the volume unless it is already at a maximum
+        Unmutes if TV is muted
+        """
         if self.status == False:
             return
         elif self.muted == True:
@@ -56,6 +78,10 @@ class Television:
                 self.volume += 1
 
     def volume_down(self):
+        """
+        Decreases the volume unless it is already at the minimum
+        Unmutes if muted
+        """
         if self.status == False:
             return
         elif self.muted == True:
@@ -71,6 +97,9 @@ class Television:
                 self.volume -= 1
 
     def __str__(self):
+        """
+        Returns status of television: Power (on or off), channel number and volume
+        """
         if self.muted == True:
             return f'Power = [{self.status}], Channel = [{self.channel}], Volume = [0]'
 

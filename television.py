a68773e3a842c40295c299ce6d39937b14ba3f54
CleverCrows <151632383+CleverCrows@users.noreply.github.com>
2024-11-22 00:04:40 -0600

    added 2 files

---
+++ television.py
@@ -0,0 +1,48 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__mute = False
+        self.__channel = Television.MIN_CHANNEL
+        self.__volume = Television.MIN_VOLUME
+    def __pow__(self, power, modulo=None):
+        if self.__status == "True":
+            self.__status = False
+        else:
+            self.__status = True
+    def __mute__(self, mute, modulo=None):
+        if self.__mute == "True":
+            self.__mute = False
+        else:
+            self.__mute = True
+    def channel_up(self):
+        if self.__mute:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__volume = self.__channel + 1
+
+    def channel_down(self):
+        if self.__mute:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__volume = self.__channel - 1
+    def volume_up(self):
+        if self.__status:
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume = self.__volume + 1
+
+    def volume_down(self):
+        if self.__mute:
+            if self.__channel < Television.MAX_VOLUME:
+                self.__volume = self.__volume - 1
+    def __str__(self):
+        '''
+        Method to return tv status
+        :return: tv status
+        '''
+        if self.__mute:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = 0"
+        else:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
